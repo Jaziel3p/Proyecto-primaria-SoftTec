5e38cd49734a5d00784bb663b4c8177d605136b5
ChuchoCarmona <108752723+ChuchoCarmona@users.noreply.github.com>
2022-10-11 14:32:19 -0500

    CHUCHO2

--- Primaria/frmBitacora.cs
+++ Primaria/frmBitacora.cs
@@ -25,7 +25,7 @@ namespace Primaria
         {
             InitializeComponent();
             CrearPDF();
-            axAcroPDF1.src = "D:/SEMESTRES/SEMESTRES/SEPTIMO SEMESTRE/Gestion de proyectos/Interfaz/PrimariaAvance8/PrimariaAvance6/Primaria/bin/Debug/Reporte_BitacoraII.pdf";
+            axAcroPDF1.src = "D:CHUCHO2-/SEMESTRES/SEMESTRES/SEPTIMO SEMESTRE/Gestion de proyectos/Interfaz/PrimariaAvance8/PrimariaAvance6/Primaria/bin/Debug/Reporte_BitacoraII.pdf";
         }
         private void CrearPDF()
         {
